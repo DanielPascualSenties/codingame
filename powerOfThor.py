b96a3f27fd055dffea319fcd0f0dde00ddc0e3cc
Daniel <danielpascualsenties@gmail.com>
2019-01-27 12:20:19 +0100

    Create powerOfThor.py

---
+++ powerOfThor.py
@@ -0,0 +1,50 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+# ---
+# Hint: You can use the debug stream to print initialTX and initialTY, if Thor seems not follow your orders.
+
+# light_x: the X position of the light of power
+# light_y: the Y position of the light of power
+# initial_tx: Thor's starting X position
+# initial_ty: Thor's starting Y position
+light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
+
+# game loop
+while True:
+    remaining_turns = int(input())  # The remaining amount of turns Thor can move. Do not remove this line.
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr)
+    if light_x > initial_tx and light_y > initial_ty:
+        initial_tx +=1;
+        initial_ty +=1;
+        print("SE")
+    elif light_x > initial_tx and light_y == initial_ty:
+        initial_tx +=1;
+        print("E")    
+    elif light_x > initial_tx and light_y < initial_ty:
+        initial_tx +=1;
+        initial_ty -=1;
+        print("NE")
+    elif light_x < initial_tx and light_y > initial_ty:
+        initial_tx -=1;
+        initial_ty +=1;
+        print("SW")
+    elif light_x < initial_tx and light_y == initial_ty:
+        initial_tx -=1;
+        print("W")    
+    elif light_x < initial_tx and light_y < initial_ty:
+        initial_tx -=1;
+        initial_ty -=1;
+        print("NW")
+    elif light_x == initial_tx and light_y > initial_ty:
+        initial_ty +=1;
+        print("S")
+    elif light_x == initial_tx and light_y < initial_ty:
+        initial_ty -=1;
+        print("N")    
+      
+    # A single line providing the move to be made: N NE E SE S SW W or NW
